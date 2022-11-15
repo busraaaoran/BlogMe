a175914fd1735009cc51badfdb5836e53009f505
Büşra <busraoran043@gmail.com>
2022-11-15 20:11:54 +0300

    angular services

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { AddArticleComponent } from './components/add-article/add-article.component';
+import { ShowArticlesComponent } from './components/show-articles/show-articles.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddArticleComponent,
+    ShowArticlesComponent,
+    AddCategoryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
